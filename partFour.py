1c07a0342086f5126af2836e37581ccf86689317
eb582 <eb582@kent.ac.uk>
2024-10-09 11:35:05 +0000

    password system

--- partFour.py
+++ partFour.py
@@ -1,1 +1,8 @@
-
+username = "Admin"
+password = "Password123"
+input_user = input("Input Username ")
+input_pass = input("Input Password ")
+if input_user == username and input_pass == password: print("Welcome Back Admin!")
+if input_user != username and input_pass == password: print("Error Access Denied")
+if input_user == username and input_pass != password: print("Incorrect Password")
+if input_user != username and input_pass != password: print("Error Access Denied")
